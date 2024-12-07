033894bb91b6da6296afd338600e8a2919b6e36b
Prasanth <prasanthprabu110@gmail.com>
2024-12-07 18:04:54 +0530

    Second commit

--- src/test/java/LearnJavaPrograms/MethodsRelatedString.java
+++ src/test/java/LearnJavaPrograms/MethodsRelatedString.java
@@ -8,7 +8,7 @@ public class MethodsRelatedString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String s= "This is the string count the duplicate string by using map";
+		String s= "This is the string count the duplicate string by using mapp";
 		String[] s1=s.split(" ");
 		int len=s1.length;
 		Map<String, Integer> map=new HashMap<String,Integer>();
---
+++ src/test/java/LearnJavaPrograms/TC001.java
@@ -0,0 +1,10 @@
+package LearnJavaPrograms;
+
+public class TC001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
